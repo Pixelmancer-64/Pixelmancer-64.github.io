d74b8ff97f8e28baee0490ce985004cfa5fbd5a2
Sotiris64 <hugobillemartins@gmail.com>
2021-09-09 09:59:10 -0300

    site pessoal 0.2

---
+++ scripts.js
@@ -0,0 +1,22 @@
+document.getElementById("lab").onclick = function () {
+    location.href = "/atividades/index.html";
+};
+document.getElementById("github").onclick = function () {
+    location.href = "https://github.com/Sotiris64";
+};
+
+let gmail = document.getElementById("gmail");
+let outlook = document.getElementById("outlook");
+let outlookText = document.getElementById("outlookText");
+let gmailText = document.getElementById("gmailText");
+
+gmail.onclick = function(){
+    let address = 'hugobillemartins@gmail.com';
+    navigator.clipboard.writeText(address);
+    gmailText.innerHTML = 'Copiado!';
+}
+outlook.onclick = function(){
+    let address = "hugobillemartins@outlook.com";
+    navigator.clipboard.writeText(address);
+    outlookText.innerHTML = 'Copiado!';
+}
