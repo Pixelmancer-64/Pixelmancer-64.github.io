b3fb914916c4d28b2e999710cdba2db1136773be
Sotiris64 <hugobillemartins@gmail.com>
2021-09-23 11:31:30 -0300

    DOM_Numeros

--- atividades/lab/DOM_Numeros/scripts.js
+++ atividades/lab/DOM_Numeros/scripts.js
@@ -31,12 +31,8 @@ biggest.onclick = function () {
 prime.onclick = function () {
     hideBoth()
     aux = inputM.value
-    
-
-    if(aux % 2 == 0) output.innerHTML = aux+' é primo';
-    
-    else output.innerHTML = aux +' NÃO é primo';
-
+    if(isPrime(aux)) output.innerHTML = aux +' é primo ';
+    else output.innerHTML = aux +' NÃO é primo ';
 
 };
 
@@ -93,3 +89,20 @@ function showAll(){
     inputR.style.visibility="visible"
 
 }
+function isPrime(result){
+    switch(result){
+        case 1: return false;
+        case 2: return true;
+        case 3: return true;
+        case 5: return true;
+    }
+    if (result % 2 == 0 || result % 3 == 0 || result % 5 == 0){
+        return false;
+    } 
+    var sqrt = Math.sqrt(result);
+    for (var i = 7; i <= sqrt; i += 6) {
+        if (result % i == 0) return false;
+        if (result % (i + 2) == 0) return false;
+    }
+    return true;
+   }
