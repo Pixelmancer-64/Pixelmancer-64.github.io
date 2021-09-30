143540c72059f77a2a4c8cf2e75979a7e3e77642
Sotiris64 <hugobillemartins@gmail.com>
2021-09-30 18:20:22 -0300

    RAIN

--- animations/Text/scripts.js
+++ animations/Text/scripts.js
@@ -4,10 +4,8 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const gradient = ctx.createLinearGradient(0,canvas.width, 0, 0)
-gradient.addColorStop('.1', 'rgba(249,215,0)');
-gradient.addColorStop('.6', 'rgba(255,0,0)');
-gradient.addColorStop('.3', 'rgba(255,165,0)');
-gradient.addColorStop('0.9', 'rgba(255,165,0)');
+gradient.addColorStop('.1', 'rgba(250,229,0)');
+gradient.addColorStop('1', '#8AFF00');
 // gradient.addColorStop('0.8', 'rgba(255,165,0)');
 // gradient.addColorStop('1', 'rgba(0,0,255)');
 
@@ -16,20 +14,19 @@ gradient.addColorStop('0.9', 'rgba(255,165,0)');
 
 
 let particlesArray = [];
-let ofsetX = .8;
-let ofsetY = .8; 
 ctx.fillStyle= 'white';
-ctx.font = '31px Verdana';
-ctx.fillText('❤', 25, 30);
-ctx.fillText('Hi!', 17, 60);
+ctx.font = '2.5em Verdana';
+ctx.textAlign = 'center';
+ctx.fillText('❤', canvas.width/40,canvas.height/30); 
 
 
-const pixelData = ctx.getImageData(0,0, 80, 80)
+
+const pixelData = ctx.getImageData(0,0, 100, 100)
 
 const mouse ={
     x: undefined,
     y: undefined,
-    radius: (canvas.height/150) * (canvas.width/150)
+    radius: (canvas.height/110) * (canvas.width/110)
 }
 
 window.addEventListener('mousemove', function(event){
@@ -80,11 +77,11 @@ class Particle{
         }else {
                 if(this.x !== this.originX){
                 let dx = this.x - this.originX;
-                this.x -= dx/10;
+                this.x -= dx/9;
                 }
                 if(this.y !== this.originY){
                     let dy = this.y - this.originY;
-                    this.y -= dy/10;
+                    this.y -= dy/9;
                     }
             }
         this.draw();
@@ -96,13 +93,15 @@ function init(){
     particlesArray = [];
         for (let y = 0, y1 = pixelData.height; y < y1; y++){
             for (let x = 0, x1 = pixelData.width; x < x1; x++)
-                if (pixelData.data[(y * 4 * pixelData.width) + (x*4) + 3] > 110){
-                    let size = 2 
+                if (pixelData.data[(y * 4 * pixelData.width) + (x*4) + 3] > 180){
+                    let ofsetX = 20;
+                    let ofsetY = 20; 
+                    let size = 1 
                     let color = gradient;
                     let mass = (Math.random()*100) + 1;
                     let posx = x * ofsetX
                     let posy = y * ofsetY
-                    particlesArray.push(new Particle(posx * 20,posy * 20,mass,size,color));
+                    particlesArray.push(new Particle(posx,posy,mass,size,color));
                 }          
         }
 }
@@ -122,7 +121,7 @@ function connect(){
             let dx = particlesArray[a].x - particlesArray[b].x;
             let dy = particlesArray[a].y - particlesArray[b].y;
             let distance = Math.sqrt(dx * dx + dy * dy);
-            if(distance < 50){
+            if(distance < 42){
                 ctx.strokeStyle=gradient;
                 ctx.lineWidth = 1;
                 ctx.beginPath();
--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-let animationsFiles = ["/animations/CircularMotion","/animations/CthulhuEyes","/animations/Dots","/animations/Text","/animations/Colorful","/animations/Fractal_Tree","/animations/Mouse_Effect"];
+let animationsFiles = ["/animations/CircularMotion","/animations/CthulhuEyes","/animations/Dots","/animations/Text","/animations/Colorful","/animations/Fractal_Tree","/animations/Mouse_Effect","/animations/Matrix","/animations/Rain"];
 document.getElementById("lab").onclick = function () {
     location.href = "/atividades/";
 };
