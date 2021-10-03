e5c7e7315ee3211f13c40f7928f26f6019b91b2f
Sotiris64 <hugobillemartins@gmail.com>
2021-10-03 11:32:19 -0300

    ascii art

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-let animationsFiles = ["/animations/CircularMotion","/animations/CthulhuEyes","/animations/Dots","/animations/Text","/animations/Colorful","/animations/Fractal_Tree","/animations/Mouse_Effect","/animations/Matrix","/animations/Rain","/animations/Fractal_Snowflake","/animations/Mouse_Trail","/animations/Image" ];
+let animationsFiles = ["/animations/CircularMotion","/animations/CthulhuEyes","/animations/Dots","/animations/Text","/animations/Colorful","/animations/Fractal_Tree","/animations/Mouse_Effect","/animations/Matrix","/animations/Rain","/animations/Fractal_Snowflake","/animations/Mouse_Trail","/animations/Image","/animations/Ascii" ];
 document.getElementById("lab").onclick = function () {
     location.href = "/atividades/";
 };
