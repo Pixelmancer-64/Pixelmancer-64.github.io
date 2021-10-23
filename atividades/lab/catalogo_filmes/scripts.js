ef0e25ed79ebef6b46fa66ee8c3e70bfc2eab22f
Sotiris64 <hugobillemartins@gmail.com>
2021-10-23 13:06:26 -0300

    v.5

--- atividades/lab/catalogo_filmes/scripts.js
+++ atividades/lab/catalogo_filmes/scripts.js
@@ -6,61 +6,65 @@ http.get('https://rafaelescalfoni.github.io/desenv_web/filmes.json', function(st
 
         let aux = '';
         let ages = [];
+
         const star = document.createElement("img");
         star.setAttribute("src","/img/star.svg");
         const starHalf = document.createElement("img");
         starHalf.setAttribute("src","/img/starHalf.svg");
+
         response.forEach(data => {
+            const showRating = document.createElement("div");
             let castAux = '<h2>Elenco</h2>';
             let generosAux = '<h2>Gênero</h2>';
             let similarAux = '<h2>Similares</h2>';
             let opinioesAux = '';
             let media = 0;
 
-            data.elenco.forEach(cast => {
-                const li = document.createElement("li");
-                li.appendChild(document.createTextNode(cast));
-                castAux += li.outerHTML;
-            });
-
-            data.generos.forEach(genre => {
-                const li = document.createElement("li");
-                li.appendChild(document.createTextNode(genre));
-                generosAux += li.outerHTML;
-            });
-
-            data.opinioes.forEach((opinion, k) => {
-                const starDiv = document.createElement("div");
-                starDiv.className = 'rating'
-                const span = document.createElement("span");
-                span.appendChild(document.createTextNode(opinion.descricao));
-                for(let i=0;i<opinion.rating;i++){
-                    starDiv.appendChild(star.cloneNode(true)); 
-                }
-                media = (media + opinion.rating)/(k+1);
-                span.appendChild(starDiv);
-                opinioesAux += span.outerHTML;
-            });
-
-            data.titulosSemelhantes.forEach((similar, k) => {
-                response.forEach(test => {
-                    if(test.id == similar) {
-                        const li = document.createElement("li");
-                        li.appendChild(document.createTextNode(test.titulo));
-                        similarAux += li.outerHTML;
-                    }
+            function eachForLoops() 
+                {data.elenco.forEach(cast => {
+                    const li = document.createElement("li");
+                    li.appendChild(document.createTextNode(cast));
+                    castAux += li.outerHTML;
                 });
-            });
-            
-            const showRating = document.createElement("div");
-            for(let i=0;i<media;i++){
+
+                data.generos.forEach(genre => {
+                    const li = document.createElement("li");
+                    li.appendChild(document.createTextNode(genre));
+                    generosAux += li.outerHTML;
+                });
+
+                data.opinioes.forEach((opinion, k) => {
+                    const starDiv = document.createElement("div");
+                    starDiv.className = 'rating'
+                    const span = document.createElement("span");
+                    span.appendChild(document.createTextNode(opinion.descricao));
+                    for(let i=0;i<opinion.rating;i++){
+                        starDiv.appendChild(star.cloneNode(true)); 
+                    }
+                    media = (media + opinion.rating)/(k+1);
+                    span.appendChild(starDiv);
+                    opinioesAux += span.outerHTML;
+                });
+
+                data.titulosSemelhantes.forEach((similar, k) => {
+                    response.forEach(test => {
+                        if(test.id == similar) {
+                            const li = document.createElement("li");
+                            li.appendChild(document.createTextNode(test.titulo));
+                            similarAux += li.outerHTML;
+                        }
+                    });
+                });
+
+                for(let i=0;i<media;i++){
                 if(media % 1 !== 0 && i+1 >= media){
                     showRating.appendChild(starHalf.cloneNode(true));
                     break;
                 }
                 showRating.className = 'showRating';
                 showRating.appendChild(star.cloneNode(true)); 
-            }
+                }
+            } eachForLoops();
 
             aux += `<div class="card">
             <div class="movie">
@@ -100,16 +104,17 @@ http.get('https://rafaelescalfoni.github.io/desenv_web/filmes.json', function(st
             </div>
             </div>
         </div>`; 
-        ages.push(data.classificacao)
 
         document.querySelector('body').innerHTML = aux;
+
+        ages.push(data.classificacao);
         });
         const movie = document.querySelectorAll('.movie');
             movie.forEach((element,k) => {
-                element.dataset.content = ages[k]
-                if (ages[k] <= 14) element.classList.toggle('special');
-                else if(ages[k] < 18) element.classList.toggle('special_yellow');
-                else if (ages[k] = 18) element.classList.toggle('special_red');
+                element.dataset.content = ages[k];
+                if (ages[k] <= 14) element.classList.toggle('green');
+                else if(ages[k] < 18) element.classList.toggle('yellow');
+                else if (ages[k] = 18) element.classList.toggle('red');
                 
             });
     }
