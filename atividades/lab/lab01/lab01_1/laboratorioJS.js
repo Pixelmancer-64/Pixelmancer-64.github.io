b89f61ee4dc24faf372c8e5ff651f6a1f207330c
Sotiris64 <hugobillemartins@gmail.com>
2021-11-04 13:46:43 -0300

    Updates

--- atividades/lab/lab01/lab01_1/laboratorioJS.js
+++
@@ -1,9 +0,0 @@
-function factorial(a){
-
-    for(i=0, result =1; i<a;i++){
-        result = result * (a-i)
-    }
-    return result;
-}
-
-console.log(factorial(12));
--- atividades/lab/lab01/lab01_2/laboratorioJS.js
+++
@@ -1,8 +0,0 @@
-function powerOf(num, times){
-
-    for(i=0, result=0; i<=times; i++){
-        result = Math.pow(num,i);
-        console.log(num + ' to the power of ' + i +' = ' + result);
-    }
-}
-powerOf(4,30);
